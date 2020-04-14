edacb62699570a451106f405ed572d9a79ad8c1c
Jo Dickson <jogreybill@gmail.com>
2020-04-14 14:20:26 -0400

    Updated version number

--- README.txt
+++ README.txt
@@ -2,7 +2,7 @@
 Contributors: ucfwebcom
 Requires at least: 5.3
 Tested up to: 5.3
-Stable tag: 0.0.0
+Stable tag: 1.0.0
 Requires PHP: 7.3
 License: GPLv3 or later
 License URI: http://www.gnu.org/copyleft/gpl-3.0.html
--- disable-yoast-seo-redirects.php
+++ disable-yoast-seo-redirects.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Disable Yoast SEO Redirects
 Description: Disables Yoast SEO Premium's redirection manager, including automatic redirection generation and notifications.
-Version: 0.0.0
+Version: 1.0.0
 Author: UCF Web Communications
 License: GPL3
 GitHub Plugin URI: UCF/Disable-Yoast-SEO-Redirects
